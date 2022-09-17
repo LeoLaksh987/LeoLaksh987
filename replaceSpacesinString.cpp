a128a0fb75409be00b552a9888d42f7706fe7035
LeoLaksh987 <98510563+LeoLaksh987@users.noreply.github.com>
2022-09-17 18:10:19 +0530

    replaceSpaces

---
+++ replaceSpacesinString.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h> 
+string replaceSpaces(string &str){
+	
+    string s="";
+    
+    for(int i=0;i<str.length();i++){
+        if(str[i]==' '){
+            s.push_back('@');
+            s.push_back('4');
+            s.push_back('0');
+        }
+        else{
+            s.push_back(str[i]);
+        }
+    }
+    return s;
+}
